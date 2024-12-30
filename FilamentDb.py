acda2eaa4e8095ae6d0660b50dd0735114494eaf
Matej Puhony <matej.puhony@seznam.cz>
2024-12-30 14:38:53 +0100

    Add transform row to spool

--- FilamentDb.py
+++ FilamentDb.py
@@ -14,7 +14,8 @@ class Filament:
 
 
 class Spool:
-    def __init__(self, id=None, filament=Filament, original_filament_weight=None, original_spool_weight=None, original_length=None):
+    def __init__(self, id=None, filament=Filament, original_filament_weight=None, original_spool_weight=None,
+                 original_length=None):
         self.id = id
         self.filament = filament
         self.original_filament_weight = original_filament_weight
@@ -175,19 +176,7 @@ class FilamentDb:
         row = cursor.fetchone()
         conn.close()
 
-        return Spool(
-                id=row[0],
-                original_filament_weight=row[2],
-                original_spool_weight=row[3],
-                original_length=row[4],
-                filament=Filament(
-                    id=row[5],
-                    manufacturer=row[6],
-                    filament_type=row[7],
-                    color_name=row[8],
-                    custom_name=row[9]
-                )
-            )
+        return self._transform_row_to_spool(row)
 
     def get_all_filaments(self):
         conn = sqlite3.connect(self.db_name)
@@ -226,19 +215,21 @@ class FilamentDb:
         conn.close()
 
         return [
-            Spool(
-                id=row[0],
-                original_filament_weight=row[2],
-                original_spool_weight=row[3],
-                original_length=row[4],
-                filament=Filament(
-                    id=row[5],
-                    manufacturer=row[6],
-                    filament_type=row[7],
-                    color_name=row[8],
-                    custom_name=row[9]
-                )
-            )
+            self._transform_row_to_spool(row)
             for row in rows
         ]
 
+    def _transform_row_to_spool(self, row):
+        return Spool(
+            id=row[0],
+            original_filament_weight=row[2],
+            original_spool_weight=row[3],
+            original_length=row[4],
+            filament=Filament(
+                id=row[5],
+                manufacturer=row[6],
+                filament_type=row[7],
+                color_name=row[8],
+                custom_name=row[9]
+            )
+        )
